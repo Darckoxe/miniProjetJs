aafde2c6bf822487b960a51ac14c5d8db349cb34
Simon JAUNET <simon.jaunet@etu.univ-nantes.fr>
2018-03-19 15:54:13 +0100

    cc

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Nom de Flickr : miniprojetjs
 
 Adresse mail Flickr : alexandredu_44@hotmail.fr
 Mot de passe Flickr : javascript
+
+CLE FLICKR A NE PAS EFFACER :
+
+Key : e33c23d5049a7016254b86a01201e648
+Secret : 55fc61d29a2fa466
---
+++ flickr/flickr.js
@@ -0,0 +1,10 @@
+function rechercheJSONFlickr(query, parPage) {
+  var url = "http://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=e33c23d5049a7016254b86a01201e648&text="+query+"&per_page="+parPage+"&format=json";
+  var images = $.getJSON(url);
+  return images;
+}
+
+$('#recherche').click(function() {
+  var res = rechercheJSONFlickr($('#nomCommune').val(), $('#nbPhotos').val());
+  console.log(res);
+})
