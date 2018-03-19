7b017c18aa791b8ec9fd5aa3843ad93d1042d7c9
Darckoxe <alexandredu_44@hotmail.fr>
2018-03-19 15:51:14 +0100

    Autocomplete ok

--- script/main.js
+++ script/main.js
@@ -1,1 +1,20 @@
+$(function (){
+  console.log("READY");
+  $("#nomCommune").autocomplete({
+    source : function(requete, reponse) {
+      $.ajax({
+        url : '/miniProjetJs/sql/commune.php',
+        type : 'GET',
+        dataType : 'json',
+        data : "commune=" + $("#nomCommune").val() + "&" +"maxRows=10",
 
+        success : function(donnee){
+          reponse($.map(donnee, function(object) {
+           return object.Ville;
+          }))
+        }
+
+      });
+    }
+  });
+});
