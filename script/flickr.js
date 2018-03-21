575a49816a53544631b1b791b63a7950744797fc
Simon JAUNET <simon.jaunet@etu.univ-nantes.fr>
2018-03-21 14:38:57 +0100

    cc bb

--- script/flickr.js
+++ script/flickr.js
@@ -1,6 +1,6 @@
 $(function() {
   function rechercheJSONFlickr(query, parPage) {
-    var url = "http://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=e33c23d5049a7016254b86a01201e648&text="+query+"&per_page="+parPage+"&format=json";
+    var url = "http://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=e33c23d5049a7016254b86a01201e648&text="+query+"&per_page="+parPage+"&format=json&nojsoncallback=1";
     var images = $.getJSON(url);
     return images;
   }
@@ -8,23 +8,31 @@ $(function() {
   $('#formRecherche').submit(function(event) {
     event.preventDefault();
     // var res = rechercheJSONFlickr($('#nomCommune').val(), $('#nbPhotos').val());
+    //
+    // // console.log(res.responseText.photos.photo[0]);
+    // console.log(res.responseJSON.photos.photo[0]);
     // console.log(res);
+
     $.ajax({
-       url : 'http://api.flickr.com/services/rest/',
-       type : 'GET',
-       dataType : 'json',
-       data : {
-         method : "flickr.photos.search",
-         api_key : "e33c23d5049a7016254b86a01201e648",
-         tags : $('#nomCommune').val(),
-         per_page : $('#nbPhotos').val(),
-         format : "json",
-       },
-       jsoncallback : 1,
-       success : function(data) {
-           $("<p>Salut</p>").appendTo("#resultats"); // On passe code_html à jQuery() qui va nous créer l'arbre DOM !
-       }
+      type: 'GET',
+      url: 'http://api.flickr.com/services/rest/',
+      data: {
+        method : "flickr.photos.search",
+        api_key : "e33c23d5049a7016254b86a01201e648",
+        tags : $('#nomCommune').val(),
+        per_page : $('#nbPhotos').val(),
+        format : "json",
+        nojsoncallback : 1
+      },
+      dataType: 'json',
+      success: function(data) {
+        console.log(data.photos.photo[0]);
+      },
+      error:function () {
+        console.error("Erreur ajax");
+      }
     });
+    // $.ajax.done()
   })
 
 })
