6a033e2c4eb47806e97dde43cf52df0a32997ec9
Simon JAUNET <simon.jaunet@etu.univ-nantes.fr>
2018-03-26 16:22:25 +0200

    salut

--- script/flickr.js
+++ script/flickr.js
@@ -30,12 +30,13 @@ $(function() {
           $('#photo_vue_liste'+id_photo).append('<div style="background-image:url(\'https://farm'+index.farm+'.staticflickr.com/'+index.server+'/'+index.id+'_'+index.secret+'.jpg\');cursor:pointer;" onclick="openModalImage(\'https://farm'+index.farm+'.staticflickr.com/'+index.server+'/'+index.id+'_'+index.secret+'.jpg\', \''+index.owner+'\', \''+index.title+'\', \''+index.id+'\');" id="photo'+id_photo+'" class="photo">');
           $('#photo_vue_liste'+id_photo).append('<div id="info'+id_photo+'" class="info">');
 
+
           $.ajax({
             type: 'GET',
             url: "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=e33c23d5049a7016254b86a01201e648&photo_id="+index.id+"&format=json&nojsoncallback=1",
             dataType: 'json',
             success: function(data) {
-              console.log(data);
+              console.log("photo : "+id_photo+" info : "+id_info);
               var arrayString = data.photo.dates.taken.split(" ");
               var arrayDate = arrayString[0].split("-");
               var date = arrayDate[2]+"/"+arrayDate[1]+"/"+arrayDate[0] + " à " + arrayString[1];
