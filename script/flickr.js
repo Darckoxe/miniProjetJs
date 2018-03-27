859b7cd6c1172b3aceb6f9926050dd3315d636cc
Darckoxe <alexandredu_44@hotmail.fr>
2018-03-27 09:33:36 +0200

    MàJ

--- script/flickr.js
+++ script/flickr.js
@@ -1,59 +1,132 @@
 $(function() {
-  function rechercheJSONFlickr(query, parPage) {
-    var url = "http://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=e33c23d5049a7016254b86a01201e648&text="+query+"&per_page="+parPage+"&format=json&nojsoncallback=1";
-    var images = $.getJSON(url);
-    return images;
-  }
-
   $('#formRecherche').submit(function(event) {
     event.preventDefault();
+    $('#resultats').empty();
+    // var nb_photos = $('#nbPhotos').val();
 
-    $.ajax({
-      type: 'GET',
-      url: 'https://api.flickr.com/services/rest/',
-      data: {
-        method : "flickr.photos.search",
-        api_key : "e33c23d5049a7016254b86a01201e648",
-        tags : $('#nomCommune').val(),
-        per_page : $('#nbPhotos').val(),
-        format : "json",
-        nojsoncallback : 1
-      },
-      dataType: 'json',
-      success: function(data) {
-        $('#resultats').empty();
-        var id_photo = 1;
-        var id_info = 1;
+    // On regarde le nombre de photos voulus.
+    // On créé le nombre de div associé
+    // On rempli les divs avec les ids qui correspondent via la variable id
 
-        data.photos.photo.forEach(function(index) {
-          $('#resultats').append('<div id="photo_vue_liste'+id_photo+'" class="photo_vue_liste">');
-          $('#photo_vue_liste'+id_photo).append('<div style="background-image:url(\'https://farm'+index.farm+'.staticflickr.com/'+index.server+'/'+index.id+'_'+index.secret+'.jpg\');cursor:pointer;" onclick="openModalImage(\'https://farm'+index.farm+'.staticflickr.com/'+index.server+'/'+index.id+'_'+index.secret+'.jpg\', \''+index.owner+'\', \''+index.title+'\', \''+index.id+'\');" id="photo'+id_photo+'" class="photo">');
-          $('#photo_vue_liste'+id_photo).append('<div id="info'+id_photo+'" class="info">');
+    var nb_photos = $('#nbPhotos').val();
+    var id = 1;
+    var tabIdPhotos = [];
 
-          $.ajax({
-            type: 'GET',
-            url: "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=e33c23d5049a7016254b86a01201e648&photo_id="+index.id+"&format=json&nojsoncallback=1",
-            dataType: 'json',
-            success: function(data) {
-              console.log(data);
-              var arrayString = data.photo.dates.taken.split(" ");
-              var arrayDate = arrayString[0].split("-");
-              var date = arrayDate[2]+"/"+arrayDate[1]+"/"+arrayDate[0] + " à " + arrayString[1];
-              $('#info'+id_info).append("<p>Titre : "+index.title+"</p>");
-              $('#info'+id_info).append("<p>Photo prise par : "+data.photo.owner.realname+"</p>");
-              $('#info'+id_info).append("<p>Photo prise le  : "+date+"</p>");
-              id_info++;
-            }
+    while (id <= nb_photos) {
+      $('#resultats').append('<div id="photo_vue_liste'+id+'" class="photo_vue_liste">');
+      $('#photo_vue_liste'+id).append('<div id="info'+id+'" class="info">');
+      $('#photo_vue_liste'+id).append('<div id="photo'+id+'" class="photo">');
+      id++;
+    }
+
+    id = 1;
+
+      $.ajax({
+        type: 'GET',
+        url: 'https://api.flickr.com/services/rest/',
+        data: {
+          method : "flickr.photos.search",
+          api_key : "e33c23d5049a7016254b86a01201e648",
+          tags : $('#nomCommune').val(),
+          per_page : nb_photos,
+          format : "json",
+          nojsoncallback : 1
+        },
+        dataType: 'json',
+
+        success: function(data) {
+          $('#barreRecherche').switchClass("barreRechercheFixed","barreRechercheNormal", 500);
+          data.photos.photo.forEach(function(index){
+             $('#photo'+id).css({
+               'background-image' : 'url(https://farm'+index.farm+'.staticflickr.com/'+index.server+'/'+index.id+'_'+index.secret+'.jpg)',
+               'background-position' : 'center',
+               'background-size' : 'cover',
+               'height' : '300px',
+               'width' : '65%',
+             });
+             tabIdPhotos.push(index.id);
+             id++;
           });
-          id_photo++;
-        });
-        $('#barreRecherche').switchClass("barreRechercheFixed","barreRechercheNormal", 500);
-      },
+          for (var i = 0; i < tabIdPhotos.length; i++) {
+            $.ajax({
+              type: 'GET',
+              async : false,
+              url: "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=e33c23d5049a7016254b86a01201e648&photo_id="+tabIdPhotos[i]+"&format=json&nojsoncallback=1",
+              dataType: 'json',
+              success: function(data) {
+                var titre = data.photo.title._content;
+                var auteur = data.photo.owner.username;
+                var arrayString = data.photo.dates.taken.split(" ");
+                var arrayDate = arrayString[0].split("-");
+                var date = arrayDate[2]+"/"+arrayDate[1]+"/"+arrayDate[0] + " à " + arrayString[1];
+                $('#info'+tabIdPhotos[i]).append("<p>Photo prise le  : "+date+"</p>");
+                $('#info'+tabIdPhotos[i]).append("<p>Titre : "+titre+"</p>");
+                $('#info'+tabIdPhotos[i]).append("<p>Photo prise par : "+auteur+"</p>");
+              },
+              error : function () {
+                console.log("erreur ajax 2");
+              }
+            });
+          }
+        },
+        error: function () {
+          console.error("Erreur ajax");
+        }
+      });
 
-      error: function () {
-        console.error("Erreur ajax");
-      }
-    });
+
+
+    // $.ajax({
+    //   type: 'GET',
+    //   url: 'https://api.flickr.com/services/rest/',
+    //   data: {
+    //     method : "flickr.photos.search",
+    //     api_key : "e33c23d5049a7016254b86a01201e648",
+    //     tags : $('#nomCommune').val(),
+    //     per_page : $('#nbPhotos').val(),
+    //     format : "json",
+    //     nojsoncallback : 1
+    //   },
+    //   dataType: 'json',
+    //   success: function(data) {
+    //     $('#resultats').empty();
+    //     var id = 1;
+    //
+    //     data.photos.photo.forEach(function(index) {
+    //       // console.log(index);
+    //       console.log(id);
+    //       $('#resultats').append('<div id="photo_vue_liste'+id+'" class="photo_vue_liste">');
+    //       $('#photo_vue_liste'+id).append('<div id="photo'+id+'" class="photo" style="background-image:url(\'https://farm'+index.farm+'.staticflickr.com/'+index.server+'/'+index.id+'_'+index.secret+'.jpg\');cursor:pointer;" onclick="openModalImage(\'https://farm'+index.farm+'.staticflickr.com/'+index.server+'/'+index.id+'_'+index.secret+'.jpg\', \''+index.owner+'\', \''+index.title+'\', \''+index.id+'\');">');
+    //       $('#photo_vue_liste'+id).append('<div style="" id="info'+id+'" class="info">');
+    //
+    //       $.ajax({
+    //         type: 'GET',
+    //         url: "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=e33c23d5049a7016254b86a01201e648&photo_id="+index.id+"&format=json&nojsoncallback=1",
+    //         dataType: 'json',
+    //         success: function(data) {
+    //           var arrayString = data.photo.dates.taken.split(" ");
+    //           var arrayDate = arrayString[0].split("-");
+    //           var date = arrayDate[2]+"/"+arrayDate[1]+"/"+arrayDate[0] + " à " + arrayString[1];
+    //           $('#info'+id_info).append("<p>Titre : "+index.title+"</p>");
+    //           $('#info'+id_info).append("<p>Photo prise par : "+data.photo.owner.realname+"</p>");
+    //           $('#info'+id_info).append("<p>Photo prise le  : "+date+"</p>");
+    //           id_info++;
+    //         }
+    //       });
+    //
+    //       id++;
+    //     });
+    //     $('#barreRecherche').switchClass("barreRechercheFixed","barreRechercheNormal", 500);
+    //   },
+    //
+    //   error: function () {
+    //     console.error("Erreur ajax");
+    //   }
+    // });
+
+
+
+
   })
 
 })
--- script/main.js
+++ script/main.js
@@ -38,7 +38,6 @@ function openModalImage(url, owner, title, id) {
     url: "https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=e33c23d5049a7016254b86a01201e648&photo_id="+id+"&format=json&nojsoncallback=1",
     dataType: 'json',
     success: function(data) {
-      console.log(data);
       var arrayString = data.photo.dates.taken.split(" ");
       var arrayDate = arrayString[0].split("-");
       var date = arrayDate[2]+"/"+arrayDate[1]+"/"+arrayDate[0] + " à " + arrayString[1];
